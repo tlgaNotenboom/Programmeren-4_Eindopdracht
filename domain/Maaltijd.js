5ba82371f220c65153a84f6820e9b21d298277dc
Thomas NUT <tlganote@avans.nl>
2018-05-16 13:48:14 +0200

    Maaltijd fully functional

--- domain/Maaltijd.js
+++ domain/Maaltijd.js
@@ -1,10 +1,30 @@
 class Maaltijd {
-    constructor(naam, beschrijving, ingredienten, allergie, prijs) {
+    constructor(naam, beschrijving, ingredienten, allergie, prijs, studentenhuisID) {
         this.naam = naam
         this.beschrijving = beschrijving
         this.ingredienten = ingredienten
         this.allergie = allergie
         this.prijs = prijs
+        this.studentenhuisID = studentenhuisID
+    }
+    getNaam() {
+        return this.naam
+    }
+    getBeschrijving() {
+        return this.beschrijving
+    }
+    getIngredienten() {
+        return this.ingredienten
+    }
+    getAllergie() {
+        return this.allergie
+    }
+    getPrijs() {
+        return this.prijs
+    }
+
+    getStudentenhuisID(){
+        return this.studentenhuisID
     }
 }
 
